e99b1289ac2ce81caf2bfeac15905796869fdd6e
Rahul Chhabra <rahulchhabra07@users.noreply.github.com>
2016-03-29 21:46:28 +0530

    Update main.cpp
    Minor bug fixes.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include < opencv2/opencv.hpp >
-#include < iostream >
-#include < vector >
-#include < algorithm >
-#include < X11/Xlib.h >
-#include < X11/Xutil.h >
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
 using namespace std;
 using namespace cv;
 
